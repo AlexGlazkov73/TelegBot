8c0ae264ddbf192573731b10c62a1393ee0ca4e7
AlexGlazkov73 <87780656+AlexGlazkov73@users.noreply.github.com>
2021-08-29 00:09:45 +0300

    Twice version

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 import telebot
-from config import keys, TOKEN
+import configparser
 from extensions import APIException, CryptoConverter
 
-bot = telebot.TeleBot(TOKEN)
+config = configparser.ConfigParser()  # создаём объекта парсера
+config.read("config.ini")
+
+token = config.get('section', 'TOKEN')
+key = config.get('section', 'keys')
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands = ['start', 'help'])
 def starting(message: telebot.types.Message):
     text = 'Чтобы начать работу бота введите команду в следующем формате: \n<имя валюты, цену которой хотите узнать> \
 <имя валюты, в которой надо узнать цену первой валюты> \
-<количество первой валюты> \
-Узнать все доступные валюты: /values'
+<количество первой валюты> \nУзнать все доступные валюты: /values'
     bot.reply_to(message, text)
 
 @bot.message_handler(commands =['values'])
 def value(message: telebot.types.Message):
     text = 'Доступные валюты:'
-    for key in keys:
-        text = '\n'.join((text, key, ))
+    for k in key:
+        text = '\n'.join((text, k, ))
     bot.reply_to(message, text)
 
 @bot.message_handler(content_types = ['text'])
@@ -24,7 +28,7 @@ def convert(message: telebot.types.Message):
     try:
         values = message.text.split()
         if len(values) != 3:
-            raise APIException('Слишком много данных')
+            raise APIException('Неправильно введен запрос')
         quote, base, amount = values
         total_base = CryptoConverter.get_price(quote, base, amount)
     except APIException as e:
@@ -32,7 +36,7 @@ def convert(message: telebot.types.Message):
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:
-        text = f'Цена {amount} {keys[quote.lower()]} в {keys[base.lower()]} - {total_base}'
+        text = f'Цена {amount} {key[quote.lower()]} в {key[base.lower()]} - {total_base}'
         bot.send_message(message.chat.id, text)
 
 bot.polling()
